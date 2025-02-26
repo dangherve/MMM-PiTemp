c885b208a15d5317f8d0bc30aa34046841279617
dangherve <dangherve@gmail.com>
2025-02-26 03:41:37 -0500

    add advance mode to correspond RPI5 fan control

--- MMM-PiTemp.js
+++ MMM-PiTemp.js
@@ -1,12 +1,21 @@
 Module.register("MMM-PiTemp", {
     defaults: {
         tempUnit: "C",
-        freq: 60000,
+        freq: 1000,
         high: 80,
         low: 70,
         highColor: "red",
         lowColor: "green",
         otherColor: "yellow",
+        advance: 0,
+        t0: 50,
+        t1: 60,
+        t2: 67.5,
+        t3: 75,
+        t0Color: "rgb(0,200,0)",
+        t1Color: "rgb(0,125,0)",
+        t2Color: "yellow",
+        t3Color: "red",
         label: "<i class='fab fa-raspberry-pi'></i>"
     },
 
@@ -34,12 +43,26 @@ Module.register("MMM-PiTemp", {
         switch (notification) {
             case "temperature":
                 var e = document.getElementById("pi_temp");
-                if ( parseFloat(payload) <= this.config.low) {
-                    e.style.color = this.config.lowColor;
-                } else if ( parseFloat(payload) >= this.config.high) {
-                    e.style.color = this.config.highColor;
-                } else {
-                    e.style.color = this.config.otherColor;
+
+                if (this.config.advance === 1){
+                    e.style.color = "rgb(60,90,120)"
+                    if ( parseFloat(payload) <= this.config.t0) {
+                        e.style.color = this.config.t0Color;
+                    } else if ( parseFloat(payload) <= this.config.t1) {
+                        e.style.color = this.config.t1Color;
+                    } else if ( parseFloat(payload) <= this.config.t2) {
+                        e.style.color = this.config.t2Color;
+                    } else {
+                        e.style.color = this.config.t3Color;
+                    }
+                }else{
+                    if ( parseFloat(payload) <= this.config.low) {
+                        e.style.color = this.config.lowColor;
+                    } else if ( parseFloat(payload) >= this.config.high) {
+                        e.style.color = this.config.highColor;
+                    } else {
+                        e.style.color = this.config.otherColor;
+                    }
                 }
 
                 var temp;
