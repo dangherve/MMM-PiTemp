6a33549eb817a868cedea843d7d3b91a433bcb34
dangherve <dangherve@gmail.com>
2025-02-26 03:39:52 -0500

    indent correction use only space not a mix tab/space

--- MMM-PiTemp.js
+++ MMM-PiTemp.js
@@ -1,55 +1,55 @@
 Module.register("MMM-PiTemp", {
-	defaults: {
-		tempUnit: "C",
-    		freq: 60000,
-    		high: 80,
-    		low: 70,
-    		highColor: "red",
-    		lowColor: "green",
-    		otherColor: "yellow",
-		label: "<i class='fab fa-raspberry-pi'></i>"
-	},
-	
-	start: function() {
-   		this.sendSocketNotification("get_temp");
-  	},
+    defaults: {
+        tempUnit: "C",
+        freq: 60000,
+        high: 80,
+        low: 70,
+        highColor: "red",
+        lowColor: "green",
+        otherColor: "yellow",
+        label: "<i class='fab fa-raspberry-pi'></i>"
+    },
 
-  	getDom: function() {
-		var e = document.createElement("div")
-   		e.id = "pi_temp"
-		return e
-	},
+    start: function() {
+        this.sendSocketNotification("get_temp");
+    },
 
- 	 notificationReceived: function(notification, payload, sender) {
-	 	switch(notification) {
-      			case "DOM_OBJECTS_CREATED":
-        		var timer = setInterval(()=>{
-				this.sendSocketNotification("get_temp")
-        		}, this.config.freq)
-        		break
-    		}
-	},
+    getDom: function() {
+        var e = document.createElement("div")
+        e.id = "pi_temp"
+        return e
+    },
 
-  	socketNotificationReceived: function(notification, payload) {
-		switch (notification) {
-			case "temperature":
-				var e = document.getElementById("pi_temp");
-				if (parseFloat(payload) <= this.config.low) {
-					e.style.color = this.config.lowColor;
-				} else if (parseFloat(payload) >= this.config.high) {
-					e.style.color = this.config.highColor;
-				} else {
-					e.style.color = this.config.otherColor;
-				}
+    notificationReceived: function(notification, payload, sender) {
+        switch(notification) {
+            case "DOM_OBJECTS_CREATED":
+                var timer = setInterval(()=>{
+                    this.sendSocketNotification("get_temp")
+                }, this.config.freq)
+                break
+            }
+    },
 
-				var temp;
-				if (this.config.tempUnit === "C") {
-					temp = payload.toString() + "°C";
-				} else {
-					temp = (payload * (9 / 5) + 32).toFixed(1).toString() + "°F";
-				}
-				e.innerHTML = this.config.label + " " + temp;
-				break;
-		}
-  	},
+    socketNotificationReceived: function(notification, payload) {
+        switch (notification) {
+            case "temperature":
+                var e = document.getElementById("pi_temp");
+                if ( parseFloat(payload) <= this.config.low) {
+                    e.style.color = this.config.lowColor;
+                } else if ( parseFloat(payload) >= this.config.high) {
+                    e.style.color = this.config.highColor;
+                } else {
+                    e.style.color = this.config.otherColor;
+                }
+
+                var temp;
+                if (this.config.tempUnit === "C") {
+                    temp = payload.toString() + "°C";
+                } else {
+                    temp = (payload * (9 / 5) + 32).toFixed(1).toString() + "°F";
+                }
+                e.innerHTML = this.config.label + " " + temp;
+                break;
+        }
+      },
 })
