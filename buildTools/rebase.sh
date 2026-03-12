90c948254c5e735a1205911ed0fbe7f3db8ad62f
dangherve <dangherve@gmail.com>
2026-03-12 13:40:38 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/ckoutavas/MMM-PiTemp.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/MMM-PiTemp.git
+git push --force --set-upstream origin master
